d306a95050dd671b7f52b18eabcb3373fb3ea5ef
Victor V. Evtushenko <victor.evtushenko@quarta.ru>
2024-02-08 11:16:59 +0300

    added catppuccin colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
